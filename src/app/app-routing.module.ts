8adfa10a24ff506ffc3665d78f4f8f7627ae0243
Tramnvph14967 <tramnvph14967@fpt.edu.vn>
2022-06-08 17:01:28 +0700

    update_home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-import { ProductAddComponent } from './components/product/product-add/product-add.component';
-import { ProductDetailComponent } from './components/product/product-detail/product-detail.component';
-import { ProductsComponent } from './components/product/products/products.component';
-
-import { AboutComponent } from './pages/about/about.component';
 import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
   { path: "", component: HomeComponent },
-  { path: "about", component: AboutComponent },
-  { path: "product", component: ProductsComponent },
-  { path: "product/add", component: ProductAddComponent },
-  { path: "product/:id", component: ProductDetailComponent },
-  { path: "product/edit/:id", component: ProductAddComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { ProductsComponent } from './components/product/products/products.component';
+
 import { FooterComponent } from './components/footer/footer.component';
 import { FormsModule } from '@angular/forms';
 import { PagesComponent } from './pages/pages.component';
-import { ProductDetailComponent } from './components/product/product-detail/product-detail.component';
-import { ProductAddComponent } from './components/product/product-add/product-add.component';
-import { AboutComponent } from './pages/about/about.component';
 import { HomeComponent } from './pages/home/home.component';
 import { HttpClientModule } from '@angular/common/http'
 import { RouterModule } from '@angular/router';
@@ -19,14 +16,8 @@ import { RouterModule } from '@angular/router';
   declarations: [
     AppComponent,
     HeaderComponent,
-    ProductsComponent,
     FooterComponent,
     PagesComponent,
-    ProductAddComponent,
-    ProductDetailComponent,
-    AboutComponent,
-    ProductAddComponent,
-    ProductDetailComponent,
     HomeComponent,
 
   ],
